5bf05a45631175143dfcb6dea61ff3ddc4006151
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-09-19 23:03:47 -0300

    Update header.tsx
    Atualização para contar o carrinho de compras, falta só estilizar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,20 +6,65 @@ import { HeaderContainer } from '../styles/components/header'
 import Link from 'next/link'
 import {Cart} from '../components/cart'
 
+export function Header() {
+  const { handleCartClick, cartCount } = useShoppingCart();
 
-export function Header(){
-  const {loadCart, handleCartClick} = useShoppingCart()
-
-  
   return (
     <>
-      
       <HeaderContainer>
-      
-        <Link href={'/'}><Image src={logoImg}  alt="" /></Link>
-        <button onClick={handleCartClick} ><Handbag size={24} weight="bold" /></button>
+        <Link href={"/"}>
+          <Image src={logoImg} alt="" />
+        </Link>
+        <div>
+          <button onClick={handleCartClick}>
+            <Handbag size={24} weight="bold" />
+          </button>
+          {cartCount > 1 ? <CartCount>{cartCount}</CartCount> : ""}
+        </div>
       </HeaderContainer>
-      
     </>
-  )
+  );
 }
+ /*
+ export const HeaderContainer = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  button: {
+    width: 48,
+    height: 48,
+    
+    border: 0,
+    borderRadius: 8,
+
+    backgroundColor: '$gray800',
+    color: '$gray300'
+  }
+})
+ 
+export const CartCount = styled("span", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "relative",
+
+  background: "#00875F",
+  border: "2px solid #121214",
+  borderRadius: "1000px",
+  width: "24px",
+  height: "24px",
+
+  flex: "none",
+  order: 1,
+  flexGrow: 0,
+  //zIndex: 1,
+
+  marginTop: '-50px',
+  marginLeft: '30px'
+});*/
